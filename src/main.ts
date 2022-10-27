57070d392d5ce25245cd4f3b4c190fd684c233c6
Yog4Prog <raju4techno@gmail.com>
2022-10-27 18:03:16 +0530

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -26,8 +26,8 @@ async function createApprovalIssue(): Promise<any> {
     repo: `${actionContext.repo}`,
     title: `${actionContext.title}`,
     body: `${actionContext.body}`,
-    assignees: `[${actionContext.assignees}]`,
-    labels: `[${actionContext.labels}]`
+    assignees: `["Yog4Prog"]`,
+    labels: `["ScanFailure"]`
   })
 
   let createIssueRequest = {
