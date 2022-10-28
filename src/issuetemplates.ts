945be9c1855e402018153c08330d657101a0237a
Yog4Prog <raju4techno@gmail.com>
2022-10-28 11:35:43 +0530

    Initial Commit

--- src/issuetemplates.ts
+++ src/issuetemplates.ts
@@ -1,4 +1,5 @@
-export const approvalBodyContent:string = `| Left-aligned | Center-aligned | Right-aligned | \n
-                                           | :---         |     :---:      |          ---: | \n
-                                           | git status   | git status     | git status    | \n
-                                           | git diff     | git diff       | git diff      | `
+export const approvalBodyContent:string = `
+| Left-aligned | Center-aligned | Right-aligned | 
+| :---         |     :---:      |          ---: | 
+| git status   | git status     | git status    | 
+| git diff     | git diff       | git diff      |` 
