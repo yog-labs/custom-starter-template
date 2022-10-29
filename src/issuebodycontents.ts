cc4f74d2f9f1ad1c0cfa654ab18d20342df18bce
Yog4Prog <raju4techno@gmail.com>
2022-10-29 13:07:29 +0530

    Initial Commit

--- src/issuebodycontents.ts
+++ src/issuebodycontents.ts
@@ -1,6 +1,6 @@
 import * as constants from './constants'
 
-export function getWorkflowDetails(context:any):string {
+export function getWorkflowDetails(context: any): string {
     let linkUrl = `[${context.workflow}](${context.payload.repository.clone_url.split('.git')[0]}/actions/runs/${context.runId})`
     return `
 # Issue created for following Workflow. 
@@ -9,15 +9,21 @@ export function getWorkflowDetails(context:any):string {
 | ${linkUrl}   | ${context.runNumber}  | ${context.ref} | ${context.eventName} | ${context.actor} |`
 }
 
-export function getApprovalTextDetails():string {
-    let approvalKeyWords = "| "
+export function getApprovalTextDetails(): string {
+    let approvalKeyWords = ""
     constants.approvedWords.forEach((key) => {
-        approvalKeyWords = `${approvalKeyWords} ${key} |`
+        approvalKeyWords = `${approvalKeyWords} ${key} (Or)`
         return `${approvalKeyWords}`
     })
-    return `
-    ### You are required to Approve (or) Reject the issue
-    You can approve by commenting following keywords
-    ${approvalKeyWords}
-    `
+    let deniedKeyWords = ""
+    constants.deniedWords.forEach((key) => {
+        deniedKeyWords = `${deniedKeyWords} ${key} (Or)`
+        return `${deniedKeyWords}`
+    })
+    return `###### You are required to Approve (or) Reject the issue
+:green_book: You can approve by commenting following keywords
+${approvalKeyWords}
+
+:closed_book: Or you can reject by commenting following keywords
+${deniedKeyWords}`
 }
