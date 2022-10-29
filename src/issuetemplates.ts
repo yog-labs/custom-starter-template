57f43314309e3c9f008e4d796f736e677b81dae4
Yog4Prog <raju4techno@gmail.com>
2022-10-29 12:00:18 +0530

    Initial Commit

--- src/issuetemplates.ts
+++ src/issuetemplates.ts
@@ -5,10 +5,10 @@ export const approvalBodyContent:string = `
 | git diff     | git diff       | git diff      |` 
 
 export function getWorkflowContext(context:any):string {
-    let linkUrl = `[Navigate: ${context.workflow}](${context.payload.repository.clone_url.split('.git')[0]}/actions/runs/${context.runId})`
+    let linkUrl = `[${context.workflow}](${context.payload.repository.clone_url.split('.git')[0]}/actions/runs/${context.runId})`
     return `
 # Issue Created for following Workflow Details 
-| WorkflowName        | Workflow#             | BranchName     | TriggerEvent         | LastCommitUser   | Link       |
-| :---                | :---:                 | :---:          | :---:                | :---:            | ---:       |
-| ${context.workflow} | ${context.runNumber}  | ${context.ref} | ${context.eventName} | ${context.actor} | ${linkUrl} |`
+| WorkflowName | Workflow#             | BranchName     | TriggerEvent         | LastCommitUser   | 
+| :---         | :---:                 | :---:          | :---:                | ---:             | 
+| ${linkUrl}   | ${context.runNumber}  | ${context.ref} | ${context.eventName} | ${context.actor} |`
 }
