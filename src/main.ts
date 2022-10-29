e8cf490ef40c8cf552782d945a76a96ea982e9f1
Yog4Prog <raju4techno@gmail.com>
2022-10-29 10:23:19 +0530

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -129,11 +129,12 @@ async function closeIssue(): Promise<any> {
 
 async function run(): Promise<void> {
   console.log("Fetching details of workflow run..");
-  console.log(`github.workflow ${github.workflow}` );
-  console.log(`github.repository ${github.repository}` );
-  console.log(`github.repositoryUrl ${github.repositoryUrl}` );
-  console.log(`github.run_id ${github.run_id}` );
-  console.log(`github.run_number ${github.run_number}` );
+  console.log(`github.workflow ${github.context.eventName}` );
+  console.log(`github.workflow ${github.context.workflow}` );
+  console.log(`github.repository ${github.context.repository}` );
+  console.log(`github.repositoryUrl ${github.context.repositoryUrl}` );
+  console.log(`github.run_id ${github.context.run_id}` );
+  console.log(`github.run_number ${github.context.run_number}` );
   
   try {
     await createApprovalIssue()
